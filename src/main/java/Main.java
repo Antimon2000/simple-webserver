b3878ddb08668e4bb113691def10e3b9e983c0ab
Antimon2000 <antymon@gmx.de>
2016-09-15 17:04:51 +0200

    Command line parsing

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,52 +6,88 @@ public class Main {
 
     private static Logger logger = LogManager.getLogger(Main.class);
 
-    public static final String CMD_DOCUMENT_ROOT    = "d";
-    public static final String CMD_THREADS_PER_CORE = "t";
-    public static final String CMD_LISTEN_PORT      = "p";
+    public static final String OPTION_DOCUMENT_ROOT     = "d";
+    public static final String OPTION_THREADS_PER_CORE  = "t";
+    public static final String OPTION_LISTEN_PORT       = "p";
 
-    public static final int    DEFAULT_LISTEN_PORT = 80;
+    public static final int    DEFAULT_LISTEN_PORT      = 80;
     public static final String DEFAULT_DOCUMENT_ROOT    = "/var/www";
     public static final int    DEFAULT_THREADS_PER_CORE = 25;
 
 
     public Main(String[] args) {
-        // Set to defaults
-        int port            = Main.DEFAULT_LISTEN_PORT;
-        int threadsPerCore  = Main.DEFAULT_THREADS_PER_CORE;
-        String documentRoot = Main.DEFAULT_DOCUMENT_ROOT;
-
         // Process command line input
         CommandLineParser parser = new DefaultParser();
 
         try {
-            CommandLine cmd = parser.parse(getCliOptions(), args);
+            CommandLine cmd = parser.parse(createOptions(), args);
 
-            if (cmd.hasOption(Main.CMD_THREADS_PER_CORE)) {
-                try {
-                    threadsPerCore = Integer.parseInt(cmd.getOptionValue(Main.CMD_THREADS_PER_CORE));
-                } catch (Exception e) {
-                    // Silently ignore and use default.
-                }
-            }
+            int listenPort      = readListenPort(cmd, Main.DEFAULT_LISTEN_PORT);
+            int threadsPerCore  = readThreadsPerCore(cmd, Main.DEFAULT_THREADS_PER_CORE);
+            String documentRoot = readDocumentRoot(cmd, Main.DEFAULT_DOCUMENT_ROOT);
+
+            // Start server
+            logger.trace("Starting server on port " + listenPort
+                    + ", serving " + documentRoot
+                    + " with "     + threadsPerCore + " threads per core"
+            );
+            //new SimpleWebserver(port, documentRoot, threadsPerCore).acceptConnections();
         } catch (ParseException e) {
             e.printStackTrace();
         }
-
-        // Start server
-        logger.trace("Starting server on port " + port
-                + ", serving " + documentRoot
-                + " with " + threadsPerCore + " threads per core"
-        );
-        //new SimpleWebserver(port, documentRoot, threadsPerCore).acceptConnections();
     }
 
 
-    private Options getCliOptions() {
+    private String readDocumentRoot(CommandLine cmd, String defaultValue) {
+        return cmd.getOptionValue(Main.OPTION_DOCUMENT_ROOT, defaultValue);
+    }
+
+
+    private int readListenPort(CommandLine cmd, int defaultValue) {
+        return readIntegerOption(cmd, Main.OPTION_LISTEN_PORT, defaultValue);
+    }
+
+
+    private int readThreadsPerCore(CommandLine cmd, int defaultValue) {
+        return readIntegerOption(cmd, Main.OPTION_THREADS_PER_CORE, defaultValue);
+    }
+
+
+    private int readIntegerOption(CommandLine cmd, String optionShort, int defaultValue) {
+        int intOption = defaultValue;
+
+        if (cmd.hasOption(optionShort)) {
+            try {
+                intOption = Integer.parseInt(cmd.getOptionValue(optionShort));
+            } catch (Exception e) {
+                // Silently ignore and use default.
+            }
+        }
+
+        return intOption;
+    }
+
+
+    private Options createOptions() {
         Options options = new Options();
-        options.addOption(Main.CMD_THREADS_PER_CORE, true, "Number of threads per core (default: " + DEFAULT_THREADS_PER_CORE + ")");
-        options.addOption(Main.CMD_DOCUMENT_ROOT, true, "This webserver's document root (default: " + DEFAULT_DOCUMENT_ROOT + ")");
-        options.addOption(Main.CMD_LISTEN_PORT, true, "Port that this server is listening for connections on (default: " + DEFAULT_LISTEN_PORT + ")");
+
+        options.addOption(
+            Main.OPTION_THREADS_PER_CORE,
+            true,
+            "Number of threads per core (default: " + DEFAULT_THREADS_PER_CORE + ")"
+        );
+
+        options.addOption(
+            Main.OPTION_DOCUMENT_ROOT,
+            true,
+            "This webserver's document root (default: " + DEFAULT_DOCUMENT_ROOT + ")"
+        );
+
+        options.addOption(
+            Main.OPTION_LISTEN_PORT,
+            true,
+            "Port that this server is listening for connections on (default: " + DEFAULT_LISTEN_PORT + ")"
+        );
 
         return options;
     }
